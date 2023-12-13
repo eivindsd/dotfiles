6669a111365470b4bd00428c4b0fac14e2b5148e
Eivind Syrdalen Dovland <eivind.dovland@gmail.com>
2023-12-13 14:22:22 +0100

    add omz symlink

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -5,3 +5,7 @@ ln -s ~/dotfiles/kitty ~/.config/kitty
 #gitconfig symlink
 rm ~/.gitconfig
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
+
+#zsh symlink
+rm ~/.zshrc
+ln -s ~/dotfiles/oh-my-zsh-custom/zshrc ~/.zshrc
